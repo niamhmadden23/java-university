bd9b999753cc7e8935f1c079da3f916fe38580d2
Niamh Madden <niamhmadden15@gmail.com>
2022-04-09 12:42:57 +0100

    char and boolean

---
+++ CharandBoolean/src/CharBoolean.java
@@ -0,0 +1,14 @@
+public class CharBoolean {
+    public static void main (String[] args){
+        // char can store a single character
+        char myChar = 'D';
+        char uniCodeChar = '\u0044';
+        System.out.println(myChar);
+        System.out.println(uniCodeChar);
+
+        boolean trueBoolean = true;
+        boolean falseBoolean = false;
+        System.out.println(trueBoolean);
+        System.out.println(falseBoolean);
+    }
+}
